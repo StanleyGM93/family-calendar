0ee245a82556bc9047aeefabbd859de4489e33e0
Stanley Mair <stanleygmair@gmail.com>
2023-10-05 12:33:08 +1300

    Updated react router with UpdateMember component

--- client/routes.tsx
+++ client/routes.tsx
@@ -4,12 +4,14 @@ import {
   Route,
 } from 'react-router-dom'
 
-import App from './components/App.js'
-import UpdateAppointment from './components/UpdateAppointment.js'
+import App from './components/App.tsx'
+import UpdateAppointment from './components/UpdateAppointment.tsx'
+import UpdateMember from './components/UpdateMember.tsx'
 
 export const routes = createRoutesFromElements(
   <Route path="/" element={<App />}>
     <Route path="/appointments/:id" element={<UpdateAppointment />} />
+    <Route path="/member/:id" element={<UpdateMember />} />
   </Route>
 )
 
