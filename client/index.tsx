e0bf8e6dd674e22ac16e7e66f0d3b7bbefedff16
Stanley Mair <stanleygmair@gmail.com>
2023-11-25 19:19:52 +1300

    Added user to index.tsx

--- client/index.tsx
+++ client/index.tsx
@@ -1,12 +1,18 @@
+import { createContext, useContext } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 import { ChakraProvider } from '@chakra-ui/react'
-import { Auth0Provider } from '@auth0/auth0-react'
-import { AuthContext } from './contexts.ts'
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react'
 
 import { router } from './routes.tsx'
+
+const UserContext = createContext<User | undefined>(undefined)
+
+export function useUser() {
+  return useContext(UserContext)
+}
 
 const queryClient = new QueryClient()
 
@@ -17,14 +23,14 @@ document.addEventListener('DOMContentLoaded', () => {
       clientId="FNfCsIaz3mOqNJUtePFquZDhV0HKfANa"
       authorizationParams={{ redirect_uri: window.location.origin }}
     >
-      {/* <AuthContext.Provider value={}> */}
-      <ChakraProvider>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-          <ReactQueryDevtools />
-        </QueryClientProvider>
-      </ChakraProvider>
-      {/* </AuthContext.Provider> */}
+      <UserContext.Provider value={useAuth0().user}>
+        <ChakraProvider>
+          <QueryClientProvider client={queryClient}>
+            <RouterProvider router={router} />
+            <ReactQueryDevtools />
+          </QueryClientProvider>
+        </ChakraProvider>
+      </UserContext.Provider>
     </Auth0Provider>
   )
 })
