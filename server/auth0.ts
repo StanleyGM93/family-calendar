83d06203493e42ce4cff451488e9a4971dc9ede0
Stanley Mair <stanleygmair@gmail.com>
2023-12-17 13:26:47 +1300

    Added checkJwt middleware to all server requests

--- server/auth0.ts
+++ server/auth0.ts
@@ -4,7 +4,6 @@ import { ParamsDictionary } from 'express-serve-static-core'
 import { JwtPayload } from 'jsonwebtoken'
 import jwks from 'jwks-rsa'
 
-// TODO: set the domain and audience (API Identifier)
 const domain = 'https://mako-stanley.au.auth0.com'
 const audience = 'https://family-calendar/api'
 
--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import express from 'express'
 import listRouter from './routes/shopping-list.ts'
 import appointmentsRouter from './routes/appointments.ts'
 import membersRouter from './routes/family-members.ts'
+import checkJwt from './auth0.ts'
 
 const server = express()
 server.use(express.json())
@@ -17,14 +18,16 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
+// Adding auth0 check to all endpoints
+server.use(checkJwt)
 // Different routes set up
 server.use('/api/v1/list', listRouter)
 server.use('/api/v1/appointments', appointmentsRouter)
 server.use('/api/v1/members', membersRouter)
 
 // Get /
-server.get('/', (req, res) => {
-  res.send('This is the landing page')
-})
+// server.get('/', (req, res) => {
+//   res.send('This is the landing page')
+// })
 
 export default server
