b6a4d48861bf9fc1e585ed6d4a20c9255b029152
Stanley Mair <stanleygmair@gmail.com>
2024-02-13 19:18:20 +1300

    wrote test for getAllAppointments func

--- server/db/__tests__/appointments-db.test.ts
+++ server/db/__tests__/appointments-db.test.ts
@@ -12,6 +12,16 @@ beforeEach(async () => {
   await connection.seed.run()
 })
 
+describe('getAllAppointments', () => {
+  it('should return an array of appointments', async () => {
+    const appointments = await db.getAllAppointments()
+
+    expect(Array.isArray(appointments)).toBe(true)
+    expect(appointments).toHaveLength(3)
+    expect(typeof appointments[0] === 'object').toBe(true)
+  })
+})
+
 //After all tests close the connection
 afterAll(async () => {
   await connection.destroy()
