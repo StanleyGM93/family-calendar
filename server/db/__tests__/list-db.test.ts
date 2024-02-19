9d6c2729463066cb2cead6dd2fc144d69995986c
Stanley Mair <stanleygmair@gmail.com>
2024-02-19 19:11:45 +1300

    Tests for getListItemById

--- server/db/__tests__/list-db.test.ts
+++ server/db/__tests__/list-db.test.ts
@@ -20,6 +20,16 @@ describe('getAllListItems', () => {
     expect(allListItems).toHaveLength(3)
   })
 })
+
+describe('getListItemById', () => {
+  it('returns an array with a single list item', async () => {
+    const listItem = await db.getListItemById(2)
+
+    expect(Array.isArray(listItem)).toBe(true)
+    expect(listItem).toHaveLength(1)
+    expect(listItem[0].id).toEqual(2)
+  })
+})
 //After all tests close the connection
 afterAll(async () => {
   await connection.destroy()
