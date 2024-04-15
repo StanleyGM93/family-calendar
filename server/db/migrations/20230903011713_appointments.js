e9b3d1dc9f82c8ad953c074c36a1eb4cc6a34b9a
Stanley Mair <stanleygmair@gmail.com>
2024-04-15 18:50:09 +1200

    Update to appts migration file

--- server/db/migrations/20230903011713_appointments.js
+++ server/db/migrations/20230903011713_appointments.js
@@ -5,7 +5,13 @@
 export function up(knex) {
   return knex.schema.createTable('appointments', (table) => {
     table.increments('id').primary()
-    // table.integer('member_id').references('id').inTable('family_members')
+    table
+      .integer('member_id')
+      .unsigned()
+      .nullable()
+      .references('id')
+      .inTable('family_members')
+      .onDelete('SET NULL')
     table.dateTime('date_time')
     table.string('location')
     table.string('purpose')
