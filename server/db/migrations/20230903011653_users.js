c58a835d28b2985f4ddd1abf49f61ce1f69a8f79
Stanley Mair <stanleygmair@gmail.com>
2024-04-16 19:03:44 +1200

    Updated users migration file

--- server/db/migrations/20230903011653_users.js
+++ server/db/migrations/20230903011653_users.js
@@ -5,10 +5,11 @@
 export function up(knex) {
   return knex.schema.createTable('users', (table) => {
     table.increments('id').primary()
-    table.string('name')
-    table.string('username').unique()
-    table.string('password')
-    table.string('email').unique()
+    table.string('name').notNullable()
+    table.string('username').unique().notNullable()
+    table.string('password').notNullable()
+    table.string('email').unique().notNullable()
+    table.timestamps(true, true) // Adds created_at and updated_at columns
   })
 }
 
