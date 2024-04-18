f06b858650f4cb14a619d2e74055400a6583358a
Stanley Mair <stanleygmair@gmail.com>
2024-04-18 18:48:34 +1200

    Improved shopping list migration

--- server/db/migrations/20230903011706_shopping-list.js
+++ server/db/migrations/20230903011706_shopping-list.js
@@ -5,9 +5,9 @@
 export function up(knex) {
   return knex.schema.createTable('shopping_list', (table) => {
     table.increments('id').primary()
-    // table.integer('user_id').references('id').inTable('users')
-    table.string('item')
-    table.integer('quantity')
+    table.integer('user_id').unsigned().references('id').inTable('users')
+    table.string('item').notNullable()
+    table.integer('quantity').notNullable()
     table.dateTime('created_at').defaultTo(knex.fn.now())
   })
 }
